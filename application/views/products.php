efd1c00d7948b636747d49fa1c269a1629476005
PAULA-IULIANA ISCU <iscu.paula@gmail.com>
2021-05-25 15:18:26 +0300

    Change localhost

--- application/views/products.php
+++ application/views/products.php
@@ -6,7 +6,7 @@
         <link rel="shortcut icon" href="#">
         <title> Products </title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-        <link rel = "stylesheet" type = "text/css"  href = "http://localhost/ShStore/css/products.css"/>
+        <link rel = "stylesheet" type = "text/css"  href = "http://localhost/CC-Store/css/products.css"/>
     </head>
     <body>
         <nav class="navbar navbar-expand-sm navbar navbar-dark bg-custom-2 ">
